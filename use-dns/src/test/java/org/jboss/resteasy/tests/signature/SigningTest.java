b0afe354ad6558a1eec229f99b0e008dc8fdcfc3
Weinan Li <l.weinan@gmail.com>
2015-05-02 10:56:48 +0800

    cleanup

--- use-dns/src/test/java/org/jboss/resteasy/tests/signature/SigningTest.java
+++ use-dns/src/test/java/org/jboss/resteasy/tests/signature/SigningTest.java
@@ -23,82 +23,77 @@ import java.security.KeyPair;
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @version $Revision: 1 $
  */
-public class SigningTest
-{
-   public static KeyPair keys;
-   public static DosetaKeyRepository repository;
-   public static Client client;
+public class SigningTest {
+    public static KeyPair keys;
+    public static DosetaKeyRepository repository;
+    public static Client client;
+    public static final String SERVER_URL = "http://localhost:8080/content-signature-example";
 
-   @BeforeClass
-   public static void setup() throws Exception
-   {
-      repository = new DosetaKeyRepository();
-      repository.setKeyStorePath("client.jks");
-      repository.setKeyStorePassword("password");
+    @BeforeClass
+    public static void setup() throws Exception {
+        repository = new DosetaKeyRepository();
+        repository.setKeyStorePath("client.jks");
+        repository.setKeyStorePassword("password");
 
-      // Use DNS to discover public keys!!!
-      repository.setUseDns(true);
-      // Point to our TEST DNS server
-      repository.setDnsUri("dns://localhost:6363");
-      repository.start();
+        // Use DNS to discover public keys!!!
+        repository.setUseDns(true);
+        // Point to our TEST DNS server
+        repository.setDnsUri("dns://localhost:6363");
+        repository.start();
 
-      configureDNS();
-      client = ClientBuilder.newClient();
-   }
+        configureDNS();
+        client = ClientBuilder.newClient();
+    }
 
-   private static EagleDNS dns;
+    private static EagleDNS dns;
 
-   public static void configureDNS() throws Exception
-   {
-      dns = new EagleDNS();
-      dns.setConfigClassPath("dns/conf/config.xml");
-      dns.start();
-   }
+    public static void configureDNS() throws Exception {
+        dns = new EagleDNS();
+        dns.setConfigClassPath("dns/conf/config.xml");
+        dns.start();
+    }
 
-   @AfterClass
-   public static void shutdownDns()
-   {
-      dns.shutdown();
-      client.close();
-   }
+    @AfterClass
+    public static void shutdownDns() {
+        dns.shutdown();
+        client.close();
+    }
 
 
-   @Test
-   public void testBasicVerification() throws Exception
-   {
-      Verifier verifier = new Verifier();
-      Verification verification = verifier.addNew();
-      verification.setRepository(repository);
+    @Test
+    public void testBasicVerification() throws Exception {
+        Verifier verifier = new Verifier();
+        Verification verification = verifier.addNew();
+        verification.setRepository(repository);
 
-      WebTarget target = client.target("http://localhost:9095/signed");
-      Invocation.Builder request = target.request();
-      request.property(Verifier.class.getName(), verifier);
-      Response response = request.get();
+        WebTarget target = client.target(SERVER_URL + "/signed");
+        Invocation.Builder request = target.request();
+        request.property(Verifier.class.getName(), verifier);
+        Response response = request.get();
 
-      System.out.println("Client: ");
-      System.out.println(response.getHeaderString(DKIMSignature.DKIM_SIGNATURE));
-      Assert.assertEquals(200, response.getStatus());
+        System.out.println("Client: ");
+        System.out.println(response.getHeaderString(DKIMSignature.DKIM_SIGNATURE));
+        Assert.assertEquals(200, response.getStatus());
 
-      // verification doesn't happen unless you try and extract the entity
-      System.out.println(response.readEntity(String.class));
-      response.close();
-   }
+        // verification doesn't happen unless you try and extract the entity
+        System.out.println(response.readEntity(String.class));
+        response.close();
+    }
 
-   @Test
-   public void testPost() throws Exception
-   {
-      WebTarget target = client.target("http://localhost:9095/signed");
-      Invocation.Builder request = target.request();
-      DKIMSignature contentSignature = new DKIMSignature();
-      contentSignature.setSelector("bill");
-      contentSignature.setDomain("client.com");
-      request.property(KeyRepository.class.getName(), repository);
+    @Test
+    public void testPost() throws Exception {
+        WebTarget target = client.target(SERVER_URL + "/signed");
+        Invocation.Builder request = target.request();
+        DKIMSignature contentSignature = new DKIMSignature();
+        contentSignature.setSelector("bill");
+        contentSignature.setDomain("client.com");
+        request.property(KeyRepository.class.getName(), repository);
 
-      request.header(DKIMSignature.DKIM_SIGNATURE, contentSignature);
-      Response response = request.post(Entity.text("hello world"));
-      Assert.assertEquals(204, response.getStatus());
-      response.close();
+        request.header(DKIMSignature.DKIM_SIGNATURE, contentSignature);
+        Response response = request.post(Entity.text("hello world"));
+        Assert.assertEquals(204, response.getStatus());
+        response.close();
 
 
-   }
+    }
 }
